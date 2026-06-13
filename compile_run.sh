353daaecdb139d0d96febccdcf6b630cfee9c9a2
Alex340001 <azs30072@yandex.ru>
2026-06-13 20:51:45 +0300

    Initial commit: source code only, v2.0 cross-platform

--- compile_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/alex340001/Документы/projects/project-csharp/Multi-LLMCodeExporter_11-06-26_InLinux/Multi-LLMCodeExporter
-dotnet run --project src/LLMCodeExporter.CLI/LLMCodeExporter.CLI.csproj -- "$@"
--- Ссылка на run-exporter.sh
+++
@@ -1,1 +0,0 @@
-/home/alex340001/Документы/projects/project-csharp/Multi-LLMCodeExporter_11-06-26_InLinux/Multi-LLMCodeExporter/run-exporter.sh
